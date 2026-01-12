d92bd105b66bb141cf0d8da105a86331bad3c407
nishad <nishad9689@gmail.com>
2026-01-12 08:45:15 +0530

    part 2 of spring security contains InMemoryAuthentication

--- src/main/java/spring/secuirty/spring_security_demo/config/ProjectSecurityConfig.java
+++ src/main/java/spring/secuirty/spring_security_demo/config/ProjectSecurityConfig.java
@@ -2,27 +2,57 @@ package spring.secuirty.spring_security_demo.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.security.authentication.password.CompromisedPasswordChecker;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.password.HaveIBeenPwnedRestApiPasswordChecker;
 
 @Configuration
 public class ProjectSecurityConfig {
 
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) {
-        //line no. 15 -> request will be access by only authenticated user
-        //http.authorizeHttpRequests(requests -> requests.anyRequest().authenticated());
         http.authorizeHttpRequests(requests -> requests
                 .requestMatchers("/myAccount","/myBalance","/myLoans","/myCards").authenticated()
                 .requestMatchers("/notices","/contact").permitAll());
-        //http.formLogin(Customizer.withDefaults());
-        /**
-         * how to disable default form Login
-         */
-//        http.formLogin(httpSecurityFormLoginConfigurer -> httpSecurityFormLoginConfigurer.disable());
-//        http.httpBasic(httpSecurityFormLoginConfigurer -> httpSecurityFormLoginConfigurer.disable());
+        http.formLogin(Customizer.withDefaults());
         http.httpBasic(Customizer.withDefaults());
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user = User.withUsername("user")
+                .password("{bcrypt}$2a$12$C/hcECftGbbSym5Zxu6k7uQwPaeAKHqkd/KalhNva9EXUCp1FnR.W") //12345
+                .authorities("read")
+                .build();
+        UserDetails admin = User.withUsername("admin")
+                .password("{bcrypt}$2a$12$KaBSDDBmtC2SiH6iuLg9POPXA/0mkMBYmXRYUvEZd/aLZ5Qinkmiq") //Admin@6643
+                .authorities("admin")
+                .build(); // in authorities any value can be given
+        return new InMemoryUserDetailsManager(user,admin); //responsible to create a user
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    /**
+     * this checks if password is compromised or not meaning if password is strong or not
+     * this is available from spring security 6.3
+     * @return
+     */
+    @Bean
+    public CompromisedPasswordChecker compromisedPasswordChecker() {
+        return new HaveIBeenPwnedRestApiPasswordChecker();
     }
 }
