401438745ede0e52e0a04b98da4e4a6eada1ca41
nishad <nishad9689@gmail.com>
2026-03-05 10:35:54 +0530

    Authorization code changes v1

--- src/main/java/spring/secuirty/spring_security_demo/config/SpringSecurityUserDetailsService.java
+++ src/main/java/spring/secuirty/spring_security_demo/config/SpringSecurityUserDetailsService.java
@@ -12,6 +12,7 @@ import spring.secuirty.spring_security_demo.repo.CustomerRepository;
 import spring.secuirty.spring_security_demo.model.Customer;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -23,7 +24,8 @@ public class SpringSecurityUserDetailsService implements UserDetailsService {
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         Customer customer = customerRepository.findByEmail(username).orElseThrow(() ->
                 new UsernameNotFoundException("User details not found for the user: "+username));
-        List<GrantedAuthority> authorities = List.of(new SimpleGrantedAuthority(customer.getRole()));
+        List<GrantedAuthority> authorities = customer.getAuthorities().stream().map(authority -> new SimpleGrantedAuthority(authority.getName())).collect(Collectors.toList());
+       // List<GrantedAuthority> authorities = List.of(new SimpleGrantedAuthority(customer.getAuthorities()));
         return new User(customer.getEmail(), customer.getPwd(),authorities);
     }
 }
--- src/main/java/spring/secuirty/spring_security_demo/model/Customer.java
+++ src/main/java/spring/secuirty/spring_security_demo/model/Customer.java
@@ -4,14 +4,17 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.sql.Date;
+import java.util.Set;
 
 @Entity
 @Table(name="customer")
@@ -40,5 +43,8 @@ public class Customer {
     @JsonIgnore
     private Date createDt;
 
+    @OneToMany(mappedBy = "customer", fetch = FetchType.EAGER)
+    @JsonIgnore
+    private Set<Authority> authorities;
 
 }
